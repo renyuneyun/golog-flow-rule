b6b70934d930b89f5146950728406d5ca6a341d0
renyuneyun (Rui ZHAO) <renyuneyun@gmail.com>
2020-03-09 13:10:22 +0000

    Add R-del for very simple version

--- flow_rule.pl
+++ flow_rule.pl
@@ -15,7 +15,16 @@ poss(del(X), S) :- attribute(X, S).
 
 attribute(X, do(A, S)) :- attribute(X, S), \+ (A = del(X)).
 
+%% helper to turn sequence of action to unordered fluents (predicates)
+rdel(X, do(A, S)) :- rdel(X, S), \+ (A = del(X)).
+
+% Golog specific
+
+proc(doDel, pi(x, ?(rdel(x)) : del(x))).
+proc(propagate, while(some(x, rdel(x)), doDel)).
+
 % restoreSitArg(attribute(X,V),S,attribute(X,V,S)). % Not necessary?
+restoreSitArg(rdel(X),S,rdel(X,S)).
 
 
 % Knowledge Base
@@ -25,5 +34,9 @@ attribute(b, s0).
 attribute(c, s0).
 attribute(d, s0).
 
-:- do(del(b), s0, S1), do(del(d), S1, Sn), attribute(X, Sn).
+rdel(b, s0).
+rdel(d, s0).
+
+% :- forall((do(propagate, s0, S), attribute(X, S)), (write(S), write(":  "), write(X), nl)).
+:- do(propagate, s0, S), attribute(X, S).
 
