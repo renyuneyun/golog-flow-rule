df333e70e1831961453b79fb05e0d8951515f6a3
renyuneyun (Rui ZHAO) <renyuneyun@gmail.com>
2020-05-10 13:59:22 +0800

    Changed ver2 to "star" representation

--- flow_rule.pl
+++ flow_rule.pl
@@ -5,28 +5,30 @@
 
 % actions
 
-primitive_action(edit1(X, Vnew)).
-primitive_action(edit2(X, V, Vnew)).
+primitive_action(edit(X, V, Vnew)).
 
-primitive_action(del1(X)).
-primitive_action(del2(X, V)).
+primitive_action(del(X, V)).
 
 % preconditions
 
-poss(edit1(X, Vnew), S) :- attribute(X, _V, S).
-poss(edit2(X, V, Vnew), S) :- attribute(X, V, S).
+poss(edit(X, *, Vnew), S) :- attribute(X, _V, S).
+poss(edit(X, V, Vnew), S) :- attribute(X, V, S).
 
-poss(del1(X), S) :- attribute(X, _V, S).
-poss(del2(X, V), S) :- attribute(X, V, S).
+poss(del(X, *), S) :- attribute(X, _V, S).
+poss(del(X, V), S) :- attribute(X, V, S).
 
 % successor-state axioms
 
 attribute(X, V, do(A, S)) :-
-              (attribute(X, V, S), \+ (
-                            A = del1(X); A = del2(X, V);
-                            (\+ (V2 = V), (A = edit1(X, V2) ; A = edit2(X, V, V2)))
-                            ));
-              (attribute(X, Vold, S), (A = edit1(X, V); A = edit2(X, Vold, V))).
+              attribute(X, V, S), \+ (
+                            A = del(X, *); A = del(X, V);
+                            (\+ (V2 = V), (A = edit(X, *, V2) ; A = edit(X, V, V2)))
+                            )
+                            .
+
+attribute(X, V, do(A, S)) :-
+              attribute(X, Vold, S), (A = edit(X, *, V); A = edit(X, Vold, V))
+              .
 
 % restoreSitArg(attribute(X,V),S,attribute(X,V,S)). % Not necessary?
 
@@ -35,10 +37,15 @@ attribute(X, V, do(A, S)) :-
 
 attribute(a, 2, s0).
 attribute(b, "123", s0).
+attribute(b, "987", s0).
 attribute(c, 45, s0).
 attribute(d, "p", s0).
 
 
-:- do(del1(b), s0, S1), do(del2(d, "p"), S2, SN), attribute(X, V, SN).
+output(S) :-
+              forall(attribute(X, V, S), writeln([X, V]))
+              .
+
+:- do(del(b, *), s0, S1), do(del(d, "p"), S1, SN), output(SN).
 % :- do(del1(b), s0, S1), do(del2(c, 43), S1, SN), do(del2(d, "p"), S2, SN), attribute(X, V, SN). % Doesn't work because del2(c, 43) can't be executed.
 
