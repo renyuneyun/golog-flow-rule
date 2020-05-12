28777ecd15642b337e3eb6a98b1e03a92198571d
renyuneyun (Rui ZHAO) <renyuneyun@gmail.com>
2020-05-12 20:49:16 +0800

    Finished "correct" version using *

--- flow_rule.pl
+++ flow_rule.pl
@@ -1,4 +1,4 @@
-:- discontiguous [attr0/4, attr1/5, obligation0/5].
+:- discontiguous [attr0/6, attr1/7, obligation0/5].
 
 % Golog interpreter
 :- [golog_swi].
@@ -7,47 +7,119 @@
 
 primitive_action(pr(Pin, Pout)).
 
-primitive_action(edit(X, V, Vnew, Pin, Pout)).
+primitive_action(edit(N, T, V, Vnew, Pin, Pout)).
 
-primitive_action(del(X, V, Pin, Pout)).
+primitive_action(del(N, T, V, Pin, Pout)).
 
 % preconditions
 
-poss(pr(Pin, Pout), S) :- attr0(X, V, Pin, S); obligation0(Ob, X, Cond, Pin, S).
+poss(pr(Pin, Pout), S) :- attr0(X, N, T, V, Pin, S); obligation0(Ob, X, Cond, Pin, S).
 
-poss(edit(X, V, Vnew, Pin, Pout), S) :- attr1(X, V, Pin, Pout, S).
-poss(edit(X, V, Vnew, Pin, *), S) :- attr1(X, V, Pin, _Pout, S).
-poss(edit(X, V, Vnew, *, Pout), S) :- attr1(X, V, _Pin, Pout, S).
-poss(edit(X, V, Vnew, *, *), S) :- attr1(X, V, _Pin, _Pout, S).
-poss(edit(X, *, Vnew, Pin, Pout), S) :- attr1(X, _V, Pin, Pout, S).
-poss(edit(X, *, Vnew, Pin, *), S) :- attr1(X, _V, Pin, _Pout, S).
-poss(edit(X, *, Vnew, *, Pout), S) :- attr1(X, _V, _Pin, Pout, S).
-poss(edit(X, *, Vnew, *, *), S) :- attr1(X, _V, _Pin, _Pout, S).
+poss(edit(N, T, V, Vnew, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, T, V, Vnew, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, T, V, Vnew, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, T, V, Vnew, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, T, *, Vnew, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, T, *, Vnew, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, T, *, Vnew, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, T, *, Vnew, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, *, V, Vnew, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, *, V, Vnew, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, *, V, Vnew, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, *, V, Vnew, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, *, *, Vnew, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, *, *, Vnew, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, *, *, Vnew, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(N, *, *, Vnew, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, T, V, Vnew, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, T, V, Vnew, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, T, V, Vnew, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, T, V, Vnew, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, T, *, Vnew, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, T, *, Vnew, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, T, *, Vnew, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, T, *, Vnew, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, *, V, Vnew, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, *, V, Vnew, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, *, V, Vnew, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, *, V, Vnew, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, *, *, Vnew, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, *, *, Vnew, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, *, *, Vnew, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(edit(*, *, *, Vnew, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
 
-poss(del(X, V, Pin, Pout), S) :- attr1(X, V, Pin, Pout, S).
-poss(del(X, V, Pin, *), S) :- attr1(X, V, Pin, _Pout, S).
-poss(del(X, V, *, Pout), S) :- attr1(X, V, _Pin, Pout, S).
-poss(del(X, V, *, *), S) :- attr1(X, V, _Pin, _Pout, S).
-poss(del(X, *, Pin, Pout), S) :- attr1(X, _V, Pin, Pout, S).
-poss(del(X, *, Pin, *), S) :- attr1(X, _V, Pin, _Pout, S).
-poss(del(X, *, *, Pout), S) :- attr1(X, _V, _Pin, Pout, S).
-poss(del(X, *, *, *), S) :- attr1(X, _V, _Pin, _Pout, S).
+poss(del(N, T, V, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, T, V, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, T, V, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, T, V, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, T, *, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, T, *, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, T, *, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, T, *, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, *, V, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, *, V, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, *, V, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, *, V, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, *, *, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, *, *, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, *, *, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(N, *, *, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, T, V, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, T, V, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, T, V, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, T, V, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, T, *, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, T, *, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, T, *, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, T, *, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, *, V, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, *, V, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, *, V, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, *, V, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, *, *, Pin, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, *, *, Pin, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, *, *, *, Pout), S) :- attr1(X, N, T, V, Pin, Pout, S).
+poss(del(*, *, *, *, *), S) :- attr1(X, N, T, V, Pin, Pout, S).
 
 % successor-state axioms
 
 obligation1(Ob, X, Cond, Pin, Pout, do(A, S)) :-
     obligation1(Ob, X, Cond, Pin, Pout, S),
     \+ (
-        attr1(X, V, Pin2, Pout2, S),
+        attr1(X, N, T, V, Pin, Pout, S),
         (
-            A = del(X, V, Pin, Pout);
-            A = del(X, V, Pin, *);
-            A = del(X, V, *, Pout);
-            A = del(X, V, *, *);
-            A = del(X, *, Pin, Pout);
-            A = del(X, *, Pin, *);
-            A = del(X, *, *, Pout);
-            A = del(X, *, *, *)
+            A = del(N, T, V, Pin, Pout);
+            A = del(N, T, V, Pin, *);
+            A = del(N, T, V, *, Pout);
+            A = del(N, T, V, *, *);
+            A = del(N, T, *, Pin, Pout);
+            A = del(N, T, *, Pin, *);
+            A = del(N, T, *, *, Pout);
+            A = del(N, T, *, *, *);
+            A = del(N, *, V, Pin, Pout);
+            A = del(N, *, V, Pin, *);
+            A = del(N, *, V, *, Pout);
+            A = del(N, *, V, *, *);
+            A = del(N, *, *, Pin, Pout);
+            A = del(N, *, *, Pin, *);
+            A = del(N, *, *, *, Pout);
+            A = del(N, *, *, *, *);
+            A = del(*, T, V, Pin, Pout);
+            A = del(*, T, V, Pin, *);
+            A = del(*, T, V, *, Pout);
+            A = del(*, T, V, *, *);
+            A = del(*, T, *, Pin, Pout);
+            A = del(*, T, *, Pin, *);
+            A = del(*, T, *, *, Pout);
+            A = del(*, T, *, *, *);
+            A = del(*, *, V, Pin, Pout);
+            A = del(*, *, V, Pin, *);
+            A = del(*, *, V, *, Pout);
+            A = del(*, *, V, *, *);
+            A = del(*, *, *, Pin, Pout);
+            A = del(*, *, *, Pin, *);
+            A = del(*, *, *, *, Pout);
+            A = del(*, *, *, *, *)
         )
     )
     .
@@ -57,54 +129,126 @@ obligation1(Ob, X, Cond, Pin, Pout, do(A, S)) :-
     A = pr(Pin, Pout)
     .
 
-attr1(X, V, Pin, Pout, do(A, S)) :-
-    attr1(X, V, Pin, Pout, S),
+attr1(X, N, T, V, Pin, Pout, do(A, S)) :-
+    attr1(X, N, T, V, Pin, Pout, S),
     \+ (
-        A = del(X, V, Pin, Pout);
-        A = del(X, V, Pin, *);
-        A = del(X, V, *, Pout);
-        A = del(X, V, *, *);
-        A = del(X, *, Pin, Pout);
-        A = del(X, *, Pin, *);
-        A = del(X, *, *, Pout);
-        A = del(X, *, *, *);
+            A = del(N, T, V, Pin, Pout);
+            A = del(N, T, V, Pin, *);
+            A = del(N, T, V, *, Pout);
+            A = del(N, T, V, *, *);
+            A = del(N, T, *, Pin, Pout);
+            A = del(N, T, *, Pin, *);
+            A = del(N, T, *, *, Pout);
+            A = del(N, T, *, *, *);
+            A = del(N, *, V, Pin, Pout);
+            A = del(N, *, V, Pin, *);
+            A = del(N, *, V, *, Pout);
+            A = del(N, *, V, *, *);
+            A = del(N, *, *, Pin, Pout);
+            A = del(N, *, *, Pin, *);
+            A = del(N, *, *, *, Pout);
+            A = del(N, *, *, *, *);
+            A = del(*, T, V, Pin, Pout);
+            A = del(*, T, V, Pin, *);
+            A = del(*, T, V, *, Pout);
+            A = del(*, T, V, *, *);
+            A = del(*, T, *, Pin, Pout);
+            A = del(*, T, *, Pin, *);
+            A = del(*, T, *, *, Pout);
+            A = del(*, T, *, *, *);
+            A = del(*, *, V, Pin, Pout);
+            A = del(*, *, V, Pin, *);
+            A = del(*, *, V, *, Pout);
+            A = del(*, *, V, *, *);
+            A = del(*, *, *, Pin, Pout);
+            A = del(*, *, *, Pin, *);
+            A = del(*, *, *, *, Pout);
+            A = del(*, *, *, *, *);
         (
             (
-                A = edit(X, V, V2, Pin, Pout);
-                A = edit(X, V, V2, Pin, *);
-                A = edit(X, V, V2, *, Pout);
-                A = edit(X, V, V2, *, *);
-                A = edit(X, *, V2, Pin, Pout);
-                A = edit(X, *, V2, Pin, *);
-                A = edit(X, *, V2, *, Pout);
-                A = edit(X, *, V2, *, *)
+                A = edit(N, T, V, V2, Pin, Pout);
+                A = edit(N, T, V, V2, Pin, *);
+                A = edit(N, T, V, V2, *, Pout);
+                A = edit(N, T, V, V2, *, *);
+                A = edit(N, T, *, V2, Pin, Pout);
+                A = edit(N, T, *, V2, Pin, *);
+                A = edit(N, T, *, V2, *, Pout);
+                A = edit(N, T, *, V2, *, *);
+                A = edit(N, *, V, V2, Pin, Pout);
+                A = edit(N, *, V, V2, Pin, *);
+                A = edit(N, *, V, V2, *, Pout);
+                A = edit(N, *, V, V2, *, *);
+                A = edit(N, *, *, V2, Pin, Pout);
+                A = edit(N, *, *, V2, Pin, *);
+                A = edit(N, *, *, V2, *, Pout);
+                A = edit(N, *, *, V2, *, *);
+                A = edit(*, T, V, V2, Pin, Pout);
+                A = edit(*, T, V, V2, Pin, *);
+                A = edit(*, T, V, V2, *, Pout);
+                A = edit(*, T, V, V2, *, *);
+                A = edit(*, T, *, V2, Pin, Pout);
+                A = edit(*, T, *, V2, Pin, *);
+                A = edit(*, T, *, V2, *, Pout);
+                A = edit(*, T, *, V2, *, *);
+                A = edit(*, *, V, V2, Pin, Pout);
+                A = edit(*, *, V, V2, Pin, *);
+                A = edit(*, *, V, V2, *, Pout);
+                A = edit(*, *, V, V2, *, *);
+                A = edit(*, *, *, V2, Pin, Pout);
+                A = edit(*, *, *, V2, Pin, *);
+                A = edit(*, *, *, V2, *, Pout);
+                A = edit(*, *, *, V2, *, *)
             ), \+ (V2 = V)
         )
     )
     .
 
-attr1(X, V, Pin, Pout, do(A, S)) :-
-    attr1(X, Vold, Pin, Pout, S),
+attr1(X, N, T, V, Pin, Pout, do(A, S)) :-
+    attr1(X, N, T, Vold, Pin, Pout, S),
     (
-        A = edit(X, Vold, V, Pin, Pout);
-        A = edit(X, Vold, V, Pin, *);
-        A = edit(X, Vold, V, *, Pout);
-        A = edit(X, Vold, V, *, *);
-        A = edit(X, *, V, Pin, Pout);
-        A = edit(X, *, V, Pin, *);
-        A = edit(X, *, V, *, Pout);
-        A = edit(X, *, V, *, *)
+        A = edit(N, T, Vold, V ,Pin, Pout);
+        A = edit(N, T, Vold, V ,Pin, *);
+        A = edit(N, T, Vold, V ,*, Pout);
+        A = edit(N, T, Vold, V ,*, *);
+        A = edit(N, T, *, V ,Pin, Pout);
+        A = edit(N, T, *, V ,Pin, *);
+        A = edit(N, T, *, V ,*, Pout);
+        A = edit(N, T, *, V ,*, *);
+        A = edit(N, *, Vold, V ,Pin, Pout);
+        A = edit(N, *, Vold, V ,Pin, *);
+        A = edit(N, *, Vold, V ,*, Pout);
+        A = edit(N, *, Vold, V ,*, *);
+        A = edit(N, *, *, V ,Pin, Pout);
+        A = edit(N, *, *, V ,Pin, *);
+        A = edit(N, *, *, V ,*, Pout);
+        A = edit(N, *, *, V ,*, *);
+        A = edit(*, T, Vold, V ,Pin, Pout);
+        A = edit(*, T, Vold, V ,Pin, *);
+        A = edit(*, T, Vold, V ,*, Pout);
+        A = edit(*, T, Vold, V ,*, *);
+        A = edit(*, T, *, V ,Pin, Pout);
+        A = edit(*, T, *, V ,Pin, *);
+        A = edit(*, T, *, V ,*, Pout);
+        A = edit(*, T, *, V ,*, *);
+        A = edit(*, *, Vold, V ,Pin, Pout);
+        A = edit(*, *, Vold, V ,Pin, *);
+        A = edit(*, *, Vold, V ,*, Pout);
+        A = edit(*, *, Vold, V ,*, *);
+        A = edit(*, *, *, V, Pin, Pout);
+        A = edit(*, *, *, V, Pin, *);
+        A = edit(*, *, *, V, *, Pout);
+        A = edit(*, *, *, V, *, *)
     )
     .
 
-attr1(X, V, Pin, Pout, do(A, S)) :-
-    attr0(X, V, Pin, S),
+attr1(X, N, T, V, Pin, Pout, do(A, S)) :-
+    attr0(X, N, T, V, Pin, S),
     A = pr(Pin, Pout)
     .
 
 % Default behaviour
 
-attr0(X, V, Pin, do(A, S)) :- attr0(X, V, Pin, S).
+attr0(X, N, T, Vold, Pin, do(A, S)) :- attr0(X, N, T, Vold, Pin, S).
 
 obligation0(Ob, X, Cond, Pin, do(A, S)) :- obligation0(Ob, X, Cond, Pin, S).
 
@@ -113,11 +257,11 @@ obligation0(Ob, X, Cond, Pin, do(A, S)) :- obligation0(Ob, X, Cond, Pin, S).
 
 % Knowledge Base
 
-attr0(a, 2, pi1, s0).
-attr0(b, "123", pi1, s0).
-attr0(c, 45, pi1, s0).
-attr0(d, "p", pi2, s0).
-attr0(e, "k", pi2, s0).
+attr0(a, name1, "int", 2, pi1, s0).
+attr0(b, name2, "str", "123", pi1, s0).
+attr0(c, name3, "int", 45, pi1, s0).
+attr0(d, name4, "str", "p", pi2, s0).
+attr0(e, name5, "str", "k", pi2, s0).
 
 obligation0(oa, a, null, pi1, s0).
 obligation0(ob, b, null, pi1, s0).
@@ -126,12 +270,13 @@ obligation0(od, d, onImport, pi2, s0).
 
 output(S) :-
       writeln(''),
-      forall(attr1(X, V, Pin, Pout, S), writeln([S, X, V, Pin, Pout])),
-      forall(obligation1(Ob, X, Cond, Pin, Pout, S), writeln([S, Ob, X, Cond, Pin, Pout])).
+      forall(attr1(X, N, T, V, Pin, Pout, S), writeln([X, N, T, V, Pin, Pout])),
+      forall(obligation1(Ob, X, Cond, Pin, Pout, S), writeln([Ob, X, Cond, Pin, Pout])).
 
 
-:- do(pr(pi2, po1), s0, S4), do(edit(d, "p", "m", *, *), S4, SN), output(SN).
-:- do(pr(pi1, po1), s0, S1), do(pr(pi2, po1), S1, S2), do(pr(pi2, po2), S2, S3), do(del(b, *, *, *), S3, S4), do(del(d, "p", *, po1), S4, SN), output(SN).
-:- do(pr(pi1, po1), s0, S1), do(pr(pi2, po1), S1, S2), do(pr(pi2, po2), S2, S3), do(del(b, *, *, *), S3, S4), do(edit(d, "p", "m", *, *), S4, SN), output(SN).
+:- do([pr(pi2, po1), edit(*, *, "P", "m", *, *)], s0, Sn), output(SN).
+:- do(pr(pi2, po1), s0, S4), do(edit(*, *, "p", "m", *, *), S4, SN), output(SN).
+:- do(pr(pi1, po1), s0, S1), do(pr(pi2, po1), S1, S2), do(pr(pi2, po2), S2, S3), do(del(name2, *, *, *, *), S3, S4), do(del(*, *, "p", *, po1), S4, SN), output(SN).
+:- do(pr(pi1, po1), s0, S1), do(pr(pi2, po1), S1, S2), do(pr(pi2, po2), S2, S3), do(del(name2, *, *, *, *), S3, S4), do(edit(*, *, "p", "m", *, *), S4, SN), output(SN).
 % :- do(del1(b), s0, S1), do(del2(c, 43), S1, SN), do(del2(d, "p"), S2, SN), attribute(X, V, SN). % Doesn't work because del2(c, 43) can't be executed.
 
