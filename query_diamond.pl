cf108eb399547e2a51392f2ed058197d183a8f7e
renyuneyun (Rui Zhao) <renyuneyun@gmail.com>
2021-03-02 21:27:36 +0000

    Update query_diamond to include delete test

--- query_diamond.pl
+++ query_diamond.pl
@@ -7,6 +7,8 @@
 
 attr(name, str, "UoE", [ai, a], s0).
 obligation(credit, [ai, a], null, ai, s0).
+attr(place, str, "Earth", [ai, z], s0).
+obligation(cite, [ai, z], null, ai, s0).
 attr(sens, col, 1, [ai, b], s0).
 obligation(hide, [ai, b], null, ai, s0).
 attr(internal-1, credit, "ZR", [ai, c], s0).
@@ -27,8 +29,8 @@ obligation(dummy, null, null, ai, s0).
 % :- do(pr(ai, [ao1, ao2]), s0, S1), output(S1),
 % 	do(end(ao1) : end(ao2), S1, S2), !, outputs(S2).
 :- do(pr(ai, [ao1, ao2]) : end(ao1) : end(ao2), s0, S1), !, outputs(S1),
-	do(pr(ao1, [bi]) : end(bi) : pr(ao2, [ci]) : end(ci), S1, S2), !,
-	do(pr(bi, [bo]) : end(bo), S2, S3), !,
+	do(pr(ao1, [bi]) : end(bi) : pr(ao2, [ci]) : end(ci), S1, S2), !, outputs(S2),
+	do(pr(bi, [bo]) :del(*, str, "Earth", *, *) : end(bo), S2, S3), !, outputs(S3),
 	do(pr(ci, [co]) : edit(*, str, "UoE", str, "University of Edinburgh", *, *) : end(co), S3, S4), !,
 	do(pr(bo, [di1]) : end(di1), S4, S5), !,
 	do(pr(co, [di2]) : end(di2), S5, S6), !,
